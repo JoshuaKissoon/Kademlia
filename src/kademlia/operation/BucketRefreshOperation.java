796e41dd9a8a5f9e0c3379c88dfd458326dd74e2
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-03-06 16:38:46 +0530

    Updated the bucket refresh operation to use 1 thread to update each bucket

--- src/kademlia/operation/BucketRefreshOperation.java
+++ src/kademlia/operation/BucketRefreshOperation.java
@@ -40,12 +40,26 @@ public class BucketRefreshOperation implements Operation
         for (int i = 1; i < NodeId.ID_LENGTH; i++)
         {
             /* Construct a NodeId that is i bits away from the current node Id */
-            NodeId current = this.localNode.getNodeId().generateNodeIdByDistance(i);
+            final NodeId current = this.localNode.getNodeId().generateNodeIdByDistance(i);
 
-            // System.out.println("Distance: " + localNode.getNodeId().getDistance(current) + " - ID: " + current);
+            /* Run the Node Lookup Operation, each in a different thread to speed up things */
+            new Thread()
+            {
+                @Override
+                public void run()
+                {
+                    try
+                    {
+                        System.out.println("Distance: " + localNode.getNodeId().getDistance(current) + " - ID: " + current);
 
-            /* Run the Node Lookup Operation */
-            new NodeLookupOperation(this.server, this.localNode, this.localNode.getNodeId()).execute();
+                        new NodeLookupOperation(server, localNode, localNode.getNodeId()).execute();
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
         }
     }
 }
