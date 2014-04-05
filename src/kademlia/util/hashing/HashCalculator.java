33f9eea09fccde66468dc98acd980e405855b8b0
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-04-05 22:39:33 +0530

    Updated HashCalculator to add MD5 hashing in there

--- src/kademlia/util/hashing/HashCalculator.java
+++ src/kademlia/util/hashing/HashCalculator.java
@@ -12,10 +12,40 @@ import java.security.NoSuchAlgorithmException;
 public class HashCalculator
 {
 
+    /**
+     * Computes the SHA-1 Hash.
+     *
+     * @param toHash The string to hash
+     *
+     * @return byte[20] The hashed string
+     *
+     * @throws java.security.NoSuchAlgorithmException
+     */
     public static byte[] sha1Hash(String toHash) throws NoSuchAlgorithmException
     {
         /* Create a MessageDigest */
         MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+        /* Add password bytes to digest */
+        md.update(toHash.getBytes());
+
+        /* Get the hashed bytes */
+        return md.digest();
+    }
+
+    /**
+     * Computes the MD5 Hash.
+     *
+     * @param toHash The string to hash
+     *
+     * @return byte[16] The hashed string
+     *
+     * @throws java.security.NoSuchAlgorithmException
+     */
+    public byte[] md5Hash(String toHash) throws NoSuchAlgorithmException
+    {
+        /* Create a MessageDigest */
+        MessageDigest md = MessageDigest.getInstance("MD5");
 
         /* Add password bytes to digest */
         md.update(toHash.getBytes());
